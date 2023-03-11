f7220d6c4af7a6a1fabfefaf44098ce68bdfe2ca
katerinakiseliova <kiseliova1985@mail.ru>
2023-03-11 18:18:26 +0300

    task 2.2

--- 2_0_0/2.0.2/Program.cs
+++ 2_0_0/2.0.2/Program.cs
@@ -2,3 +2,16 @@
 //случайное число из отрезка [10, 99] и показывает
 //наибольшую цифру числа
 
+Console.WriteLine("Введите ваше число");
+int MaxNum(int num)
+{
+Console.WriteLine(num);
+int num1 = num/10;
+int num2 = num % 10;
+if ( num > num2 ) return num1;
+return num2;
+
+}
+
+int result = MaxNum(new Random().Next(10,100));
+Console.WriteLine(result);
