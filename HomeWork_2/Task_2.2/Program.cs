7127ef80ffc2c10df0c83481fefd5c6d348edb82
katerinakiseliova <kiseliova1985@mail.ru>
2023-03-12 19:01:37 +0300

    H_W 2.2

---
+++ HomeWork_2/Task_2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая выводит
+//случайное трёхзначное число и удаляет вторую цифру
+//этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+string SecondNum(int num)
+{
+    if ((-1000 < num && num < -99)|| (num < 1000 && num > 99));
+        return $"{num} -> {num / 100 * 10 + num % 10}";
+    return "The number is not three-digit!";
+
+}
+Console.WriteLine(SecondNum(int.Parse(Console.ReadLine()!)));
