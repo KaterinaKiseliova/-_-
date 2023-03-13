55a63ba27353e4c62004b068bd6fc9a0a5d09d43
katerinakiseliova <kiseliova1985@mail.ru>
2023-03-13 09:33:03 +0300

    HW_3.1

---
+++ HomeWork_3/3.1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход
+//пятизначное число и проверяет, является ли оно палиндромом
+
+
+string Palindrome(int n)
+{
+    int num = 10000;
+    if (n > 100000 || n < 10000)
+        return "число не является пятизначным!";
+    else
+        while (num > 1)
+        {
+            if (n / num % 10 != n % 10)
+                return "не является палиндромом";
+            n /=10;
+            num /=100;    
+        }
+
+    return "являетя палиндромом";
+
+ }
+  
+Console.WriteLine(Palindrome(int.Parse(Console.ReadLine()!)));
